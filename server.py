60e75aaff8b2a3e5f38f5e558eeb52def763340b
Adi Satheesh <adisatheesh4@gmail.com>
2024-04-21 01:33:56 -0400

    Added questiong generation to backend

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, send_from_directory
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api
 from flask_cors import CORS #Comment on deployment
 
 from utils.parse import parse_text
 from utils.gptsummarizer import generate_summary
+from utils.gptsummarizer import generate_questions
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #Comment on deployment
@@ -14,12 +15,21 @@ def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/api/upload", methods = ['POST'])
-async def upload_file():
+async def summarize():
     file = request.files['file']
     text = parse_text(file)
     if len(text) == 0:
         return "Invalid file type", 401
     response = await generate_summary(text)
+    return response
+
+@app.route("api/questions", methods = ['POST'])
+async def gen_questions():
+    file = request.files['file']
+    text = parse_text(file)
+    if len(text) == 0:
+        return "Invlaid file type", 401
+    response = await generate_questions(text)
     return response
 
 if __name__ == '__main__':
--- utils/gptsummarizer.py
+++ utils/gptsummarizer.py
@@ -13,6 +13,21 @@ api_key = config.get('Key', 'OPENAI_API_KEY')
 async def generate_summary(text):
     global api_key
     aiClient = OpenAI(api_key=api_key)
+    messages = [{"role": "system", "content": "You are an assistant who summarizes and details the important bits of different files. You will be given the text from a powerpoint or pdf. You don't need to summarize every slide. Just mention the important bits and what slides they are on. Output your response in html format. Ex: <div> <h1>Summary of Document </h1> <p>example text</p> </div>"}]
+    messages = formatText(messages, text)
+    try:
+        response = aiClient.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages = messages
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"Error generating response: {e}")
+
+async def generate_questions(text):
+    global api_key
+    aiClient = OpenAI(api_key=api_key)
+    messages = [{"role": "system", "content": "You are an assistant who generates questions based off of the provided text. You will be given the text from a powerpoint or pdf. Generate a list of questions in json format."}]
     messages = formatText(text)
     try:
         response = aiClient.chat.completions.create(
@@ -23,12 +38,9 @@ async def generate_summary(text):
     except Exception as e:
         print(f"Error generating response: {e}")
 
-
 def formatFile(text):
     return {"role": "user", "content": text}
 
-def formatText(text):
-    messages = [{"role": "system", "content": "You are an assistant who summarizes and details the important bits of different files. You will be given the text from a powerpoint. You don't need to summarize every slide. Just mention the important bits and what slides they are on. Output your response in html format. Ex: <div> <h1>Summary of Document </h1> <p>example text</p> </div>"}]
-    messages.append(formatFile(text))
-    return messages
+def formatText(messages, text):
+    return messages.append(formatFile(text))
 
