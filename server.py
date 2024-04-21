953f13e86a02c37b502273ff84ffe59af52b2880
Adi Satheesh <adisatheesh4@gmail.com>
2024-04-20 23:49:09 -0400

    Refactored entire project

--- server.py
+++ server.py
@@ -1,31 +1,25 @@
-from flask import Flask, request
-from flask_cors import CORS
+from flask import Flask, request, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #Comment on deployment
+
 from utils.parse import parse_text
 from utils.gptsummarizer import generate_summary
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #Comment on deployment
+api = Api(app)
 
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-@app.route('/register')
-def home():
-    return "Hello, Flask!"
-
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/api/upload", methods = ['POST'])
 async def upload_file():
     file = request.files['file']
-
     text = parse_text(file)
-
     if len(text) == 0:
         return "Invalid file type", 401
-
     response = await generate_summary(text)
-
-    print("Returning generated response")
-
     return response
 
 if __name__ == '__main__':
